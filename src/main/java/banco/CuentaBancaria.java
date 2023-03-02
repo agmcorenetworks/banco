fa7a53da543e832a8b3feeca88bca59faa2db3ac
alexscamg <alejandrosanchezsk8@gmail.com>
2023-03-02 21:13:18 +0100

    Cambio alejandro_sanchez

--- src/main/java/banco/CuentaBancaria.java
+++ src/main/java/banco/CuentaBancaria.java
@@ -1,5 +1,6 @@
 package banco;
 
+import proyectoPrueba.IMCException;
 import proyectoPrueba.Persona;
 
 public class CuentaBancaria {
@@ -7,11 +8,25 @@ public class CuentaBancaria {
 	int saldo;
 	Persona titular;
 	
+	
+	public int meterSaldo(int saldo) {
+		return this.saldo = saldo;
+	}
+	
 	public int getSaldo() {
 		return saldo;
 	}
 	
-			
+	public int retirarDinero(int retirada) throws ExceptionSaldo {
+		
+		if (retirada<saldo) {
+			int saldoRestante = saldo - retirada;
+			return saldoRestante;
+		}else {
+			throw new ExceptionSaldo("saldo menor al importe a retirar");
+		}
+	}
 	
+		
 
 }
---
+++ src/main/java/banco/ExceptionSaldo.java
@@ -0,0 +1,13 @@
+package banco;
+
+public class ExceptionSaldo extends Exception{
+	
+
+
+
+		public String msj;
+
+		public ExceptionSaldo(String msj){
+			super(msj);
+		}
+}
