178038bd03f95a724969392ac8c8550352484ad4
maalvrei <miguemorisco@gmail.com>
2023-03-03 19:46:50 +0100

    modificado ejercicio caracter

--- src/main/java/paqueteTeclado/LeerPorTeclado.java
+++ src/main/java/paqueteTeclado/LeerPorTeclado.java
@@ -3,41 +3,35 @@ package paqueteTeclado;
 import java.util.Scanner;
 
 public class LeerPorTeclado {
-	
-    private char caracter;
 
-    public char getChar() {
-        return caracter;
-    }
+	private char caracter;
 
-    public void tipoDeCaracter() throws ExcepcionDeVocal, ExcepcionDeNumero, ExcepcionDeBlanco, ExcepcionDeSalida {
-    	
-    	System.out.println("Introduce un caracter");
-    	
-    	Scanner scanner = new Scanner(System.in);
-    	
-    	String textoIntroducido = scanner.next();
-    	
-    	caracter = textoIntroducido.charAt(0);	
-    	
-        if (Character.isDigit(caracter)) {
-        	
-            throw new ExcepcionDeNumero("El caracter es un numero.");
-            
-        } else if (Character.isWhitespace(caracter)) {
-        	
-            throw new ExcepcionDeBlanco("El caracter es un espacio en blanco.");
-            
-        } else if (Character.toLowerCase(caracter) == 'a' || Character.toLowerCase(caracter) == 'e' || 
-                   Character.toLowerCase(caracter) == 'i' || Character.toLowerCase(caracter) == 'o' || 
-                   Character.toLowerCase(caracter) == 'u') {
-        	
-            throw new ExcepcionDeVocal("El caracter es una vocal.");
-            
-        } else if (caracter == 'x') {
-        	
-            throw new ExcepcionDeSalida("Has introducido el caracter de salida 'x'.");
-            
-        }
-    }
+	public char getCaracter() {
+		return caracter;
+	}
+
+	public void evaluaCaracter() throws ExcepcionDeBlanco, ExcepcionDeNumero, ExcepcionDeSalida, ExcepcionDeVocal {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Introduce un caracter.\nSi introduces mas de un caracter, se tendra en cuenta solo el primero");
+		
+		caracter = scanner.nextLine().charAt(0);
+
+		if (Character.isWhitespace(caracter)) {
+			throw new ExcepcionDeBlanco ("Has introducido un espacio en blanco");
+		} else if (Character.isDigit(caracter)) {
+			throw new ExcepcionDeNumero ("Has introducido un numero");
+		} else if ((Character.toLowerCase(caracter) == 'a' || Character.toLowerCase(caracter) == 'e'
+				|| Character.toLowerCase(caracter) == 'i' || Character.toLowerCase(caracter) == 'o'
+				|| Character.toLowerCase(caracter) == 'u')) {
+			throw new ExcepcionDeVocal ("Has introducido una vocal");
+		} else if (caracter == 'x') {
+			throw new ExcepcionDeSalida ("Has introducido el valor de salida 'x'");
+		} else {
+			System.out.println("Has introducido un caracter que no es una vocal ni un numero.");
+		}
+		
+	}
+
 }
--- src/test/java/paqueteTeclado/PruebaLeerPorTeclado.java
+++ src/test/java/paqueteTeclado/PruebaLeerPorTeclado.java
@@ -4,22 +4,24 @@ import org.junit.Test;
 
 public class PruebaLeerPorTeclado {
 	
-    @Test
-    public void pruebaLecturaCaracteres() throws ExcepcionDeVocal, ExcepcionDeNumero, ExcepcionDeBlanco, ExcepcionDeSalida {
-        LeerPorTeclado teclado = new LeerPorTeclado();
-        boolean terminar = true;
-        while (terminar)
-            try {
-                teclado.tipoDeCaracter();
-            } catch (ExcepcionDeVocal e) {
-                System.out.println(e.getMessage());
-            } catch (ExcepcionDeNumero e) {
-                System.out.println(e.getMessage());
-            } catch (ExcepcionDeBlanco e) {
-                System.out.println(e.getMessage());
-            } catch (ExcepcionDeSalida e) {
-                System.out.println(e.getMessage());
-                terminar = false;
-            }
-        }
-    }
+	
+	@Test
+	public void vamosAProbar () throws ExcepcionDeBlanco, ExcepcionDeNumero, ExcepcionDeVocal, ExcepcionDeSalida {
+		LeerPorTeclado l1 = new LeerPorTeclado();
+		while (true) {
+			try {
+				l1.evaluaCaracter();
+			} catch (ExcepcionDeVocal e) {
+				System.out.println(e.getMessage());
+			} catch (ExcepcionDeBlanco e) {
+				System.out.println(e.getMessage());
+			} catch (ExcepcionDeNumero e) {
+				System.out.println(e.getMessage());
+			} catch (ExcepcionDeSalida e) {
+				System.out.println(e.getMessage());
+				break;
+			}
+		}
+	}
+	
+}
